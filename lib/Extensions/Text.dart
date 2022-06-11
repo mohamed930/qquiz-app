a478e918890a043c01e4cb2ab20e38bc1be608c8
mohamed930 <mohammedali12477@gmail.com>
2022-06-11 12:59:06 +0200

    Add constructor to custom Text commit

--- lib/Extensions/Text.dart
+++ lib/Extensions/Text.dart
@@ -3,7 +3,11 @@ import 'package:fluttertest/Extensions/HexColor.dart';
 
 class text extends StatelessWidget {
 
+  // MARK:- TODO:- this section For initialise new varible here.
   var title;
+
+  //MARK:- TODO:- this constructor to pick text value.
+  text({this.title});
 
   @override
   Widget build(BuildContext context) {
